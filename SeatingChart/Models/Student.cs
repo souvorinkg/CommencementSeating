11d3fc5d491f402664d9c3b346c6f7bd966eaa9b
Noah Chaffin <ChaffinNC@hendrix.edu>
2023-11-17 12:25:37 -0600

    Chart and Seat Creation

--- SeatingChart/Models/Student.cs
+++ SeatingChart/Models/Student.cs
@@ -1,7 +1,14 @@
 namespace SeatingChart.Models
 {
     public class Student
-    {
+    { 
+
+        public enum Type {
+            Bachelor,
+            Graduate,
+            Empty,
+            Faculty
+        }
         public int ID { get; set; }
         public string FirstName { get; set; }  
         
